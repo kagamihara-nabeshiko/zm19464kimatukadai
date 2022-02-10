9e0748dcb039141fe03ebe87baa4bae69f69f47e
kagamihara-nabeshiko <kosaka0227sunshine@gmail.com>
2022-02-10 04:58:01 +0000

    0210

--- hubuki.js
+++ hubuki.js
@@ -1,2 +1,2 @@
-function imgchange2 () {document.getElementById ( 'logo2') .src = './ image / 白上フブキ2.jpg';}
-function bthrees2 () {document.getElementById ( 'logo2') .src = './ image / FBK.jpg';}
+function imgchange2 () {document.getElementById ('logo2') .src = './ image / 白上フブキ2.jpg';}
+function bthrees2 () {document.getElementById ('logo2') .src = './ image / FBK.jpg';}
