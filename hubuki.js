f1654ff1bc891bd1983da592babdf1045a79e88c
kagamihara-nabeshiko <kosaka0227sunshine@gmail.com>
2022-02-10 04:16:29 +0000

    0210

--- hubuki.js
+++ hubuki.js
@@ -1,2 +1,2 @@
-function imgchange2 () {document.getElementById ( 'logo2') .src = './ image / 白上フブキ.jpg';}
-function bthrees2 () {document.getElementById ( 'logo2') .src = './ image / 白上フブキ2.jpg';}
+function imgchange2 () {document.getElementById ( 'logo2') .src = './ image / 白上フブキ2.jpg';}
+function bthrees2 () {document.getElementById ( 'logo2') .src = './ image / 白上フブキ.jpg';}
--- karolabo.js
+++ karolabo.js
@@ -1,2 +1,2 @@
-function imgchange6(){document.getElementById ( 'logo6').src = '。/ image / かろらぼ.jpg';}
-function bthrees6(){document.getElementById ( 'logo6').src = '。/ image / かろらぼ2.jpg';}
+function imgchange6(){document.getElementById ( 'logo6').src = '。/ image / かろらぼ2.jpg';}
+function bthrees6(){document.getElementById ( 'logo6').src = '。/ image / かろらぼ.jpg';}
