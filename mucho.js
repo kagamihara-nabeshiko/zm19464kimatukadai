716b3ab45d9630c239c590161db776ccdbac9e20
kagamihara-nabeshiko <kosaka0227sunshine@gmail.com>
2022-02-11 13:15:03 +0000

    0211

--- mucho.js
+++ mucho.js
@@ -1,2 +1,2 @@
 function imgchange5 () {document.getElementById('logo5').src = '。/ image / mucho2.jpg';}
-function bthrees () {document.getElementById('logo5').src = '。/ image / mucho.jpg';}
+function bthrees5 () {document.getElementById('logo5').src = '。/ image / mucho.jpg';}
