e1aa679bd6169d3b22c8745ebbbe33c6a62cbd49
kagamihara-nabeshiko <kosaka0227sunshine@gmail.com>
2022-02-11 12:50:08 +0000

    0211

--- mucho.js
+++ mucho.js
@@ -1,2 +1,2 @@
-function imgchange5(){document.getElementById('logo5').src = '。/ image / ムーチョ2.jpg';}
-function bthrees5(){document.getElementById('logo5').src = '。/ image / ムーチョ.jpg';}
+function imgchange5 () {document.getElementById('logo5').src = '。/ image / ムーチョ2.jpg';}
+function bthrees5 () {document.getElementById('logo5').src = '。/ image / ムーチョ.jpg';}
--- noel.js
+++ noel.js
@@ -1,2 +1,2 @@
-function imgchange3(){document.getElementById('logo3').src = '。/ image / 白銀ノエル2.jpg';}
-function bthrees3(){document.getElementById('logo3').src = '。/ image / 白銀ノエル.jpg';}
+function imgchange3 () {document.getElementById('logo3').src = '。/ image / 白銀ノエル2.jpg';}
+function bthrees3 () {document.getElementById('logo3').src = '。/ image / 白銀ノエル.jpg';}
